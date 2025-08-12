d481685aa4a1be448c2b5464dc2f1a5439927079
bahati <bahatifidelis2004@gmail.com>
2025-08-12 15:19:28 +0300

    complete Task challenge

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Components from './Finaldocs/10-TASKS';
+
+function App() {
+  return (
+    <div className="App">
+    
+      <Components />
+      
+    </div>
+  );
+}
+
+export default App;
